ad57574d0dcf4cadb7ce438d69fe025c9a38c9b5
padfoot9445 <yipy214@gmail.com>
2026-08-11 13:35:32 +0100

    annotated ultrasound

--- sensors.py
+++ sensors.py
@@ -4,6 +4,6 @@ class Ultrasound:
     def __init__(self, trigger_pin: int, echo_pin: int):
         self.trigger_pin = trigger_pin
         self.echo_pin = echo_pin
-    def get_distance(self):
+    def get_distance(self) -> int:
         return arduino.measure_ultrasound_distance(self.trigger_pin, self.echo_pin)
     
