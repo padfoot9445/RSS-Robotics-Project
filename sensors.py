a04f0fc9ac524b588c3ea6e9ff1e3154d0b02749
padfoot9445 <yipy214@gmail.com>
2026-08-12 17:53:33 +0100

    feat/sensors

--- sensors.py
+++ sensors.py
@@ -18,7 +18,12 @@ class Ultrasound:
         return arduino.measure_ultrasound_distance(self.trigger_pin, self.echo_pin)
 
 @dataclass
-class Position:
+class RobotOrientation:
+    orientation: float # angle in radians, vertically upwards is 0, right is pi/2, left is -pi/2
+    error: float | None = field(default=None)
+
+@dataclass
+class RobotCoordinate:
     """Bottom-left is 0, 0, horizontal x, vertical y, measured in millimeters (mm)"""
     x: int
     y: int
@@ -29,11 +34,19 @@ class Position:
         if self.error_x is None or self.error_y is None:
             return None
         return math.sqrt((self.error_x**2 + self.error_y**2))
+
+@dataclass
+class RobotPosition:
+    coordinate: RobotCoordinate
+    orientation: RobotOrientation
 @dataclass
 class MarkerPosition:
     x: int
     y: int
-    angle: float # upwards is zero, rightwards is 90 degrees, but this is measured in radians
+    angle_radians: float # upwards is zero, rightwards is 90 degrees, but this is measured in radians
+    @property
+    def angle_degrees(self):
+        math.degrees(self.angle_radians)
 
 class Camera:
     def __init__(self):
@@ -43,7 +56,7 @@ class Camera:
 
     def get_markers(self):
         return vision.detect_markers() #type: ignore
-    def calculate_position(self, id: set[int] | None = None) ->  Position | None:
+    def calculate_coordinate(self, id: set[int] | None = None) ->  RobotCoordinate | None:
         markers = self.get_markers()
         if len(markers) == 0:
             return None
@@ -62,13 +75,13 @@ class Camera:
         best_x = statistics.median(x_coordinates)
         best_y = statistics.median(y_coordinates)
 
-        return Position(round(best_x), round(best_y), x_error, y_error)
+        return RobotCoordinate(round(best_x), round(best_y), x_error, y_error)
 
     def get_marker_coordinate(self, marker_id: int) -> MarkerPosition:
         coord = self.markers_positions[str(marker_id)]
         return MarkerPosition(coord[0], coord[1], math.radians(coord[2]))
 
-    def _calculate_position(self, marker: Marker) -> Position:
+    def _calculate_position(self, marker: Marker) -> RobotCoordinate:
         marker_position = self.get_marker_coordinate(marker.id)
 
         horizontal_angle = marker.position.horizontal_angle
@@ -82,9 +95,9 @@ class Camera:
 
     
     
-    def _rotate_naive_position(self, naive_x: float, naive_y: float, marker_position: MarkerPosition) -> Position:
-        sin = math.sin(math.pi + marker_position.angle)
-        cos = math.cos(math.pi + marker_position.angle)
+    def _rotate_naive_position(self, naive_x: float, naive_y: float, marker_position: MarkerPosition) -> RobotCoordinate:
+        sin = math.sin(math.pi + marker_position.angle_radians)
+        cos = math.cos(math.pi + marker_position.angle_radians)
         a = marker_position.x
         b = marker_position.y
 
@@ -110,7 +123,28 @@ class Camera:
         result_vector_list = result_vector.tolist()
         result_vector_list = [round(x) for x in result_vector_list]
 
-        return Position(result_vector_list[0], result_vector_list[1])
+        return RobotCoordinate(result_vector_list[0], result_vector_list[1])
+
+    
+    def current_angle_marker(self, marker: Marker):
+        yaw = marker.orientation.yaw
+        angle = self.get_marker_coordinate(marker.id).angle_radians
+
+        neg_yaw = -yaw
+
+        return angle + neg_yaw
+
+    def get_current_angle(self) -> RobotOrientation:
+        orientations = [self.current_angle_marker(marker) for marker in self.get_markers()]
+        error = max(orientations) - min(orientations)
+        best_orientation = statistics.median(orientations)
+        return RobotOrientation(best_orientation, error)
+
+
+
+
+
+
 
 
 
