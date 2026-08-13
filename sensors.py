5c1b8749fede272f482fe097f37b5fb64b642891
padfoot9445 <yipy214@gmail.com>
2026-08-13 09:51:54 +0100

    try reflect markers

--- sensors.py
+++ sensors.py
@@ -87,9 +87,15 @@ class Camera:
     def get_marker_coordinate(self, marker_id: int) -> MarkerPosition:
         coord = self.markers_positions[str(marker_id)]
         marker_degrees = coord[2]
+        marker_coordinates = np.array([coord[0], coord[1]])
         if self.reversed:
             marker_degrees = (marker_degrees + 180) % 360
-        return MarkerPosition(coord[0], coord[1], math.radians(marker_degrees))
+            rotation_matrix = np.array([
+                [0, -1],
+                [-1, 0]
+            ])
+            marker_coordinates = rotation_matrix @ marker_coordinates
+        return MarkerPosition(marker_coordinates[0], marker_coordinates[1], math.radians(marker_degrees))
     # TODO: Handle when to be reversed
 
     def _calculate_position(self, marker: Marker) -> RobotCoordinate:
